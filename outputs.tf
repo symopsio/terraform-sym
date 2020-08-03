cb26494af00500ad58889f6a26e0e8e1c29d3367
Chris Jester-Young <chris@symops.io>
2020-08-03 10:07:24 -0400

    First very rough cut at a top-level module, containing lambda-approvals only.

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "lambda_approve_function_arn" {
+  description = "The ARN of the approve lambda function"
+  value       = module.lambda_approvals.approve_function_arn
+}
+
+output "lambda_expire_function_arn" {
+  description = "The ARN of the expire lambda function"
+  value       = module.lambda_approvals.expire_function_arn
+}
+
+output "sym_execute_role_arn" {
+  description = "The ARN of the cross-account invocation role"
+  value       = module.lambda_approvals.sym_execute_role_arn
+}
