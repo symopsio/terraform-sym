b7b878231d410485bf88d96baacf1007c41c5d0f
Jon Bass <jon@symops.io>
2020-08-03 21:15:06 -0400

    Add to e2e

--- examples/e2e-lambda-okta-approvals/main.tf
+++ examples/e2e-lambda-okta-approvals/main.tf
@@ -23,6 +23,38 @@ module "lambda_approvals" {
   okta_org_url             = var.okta_org_url
 }
 
+# Let principals from the calling user's account ID assume roles
+data "aws_iam_policy_document" "current_account_assume_role" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+  }
+}
+
+resource "aws_iam_role" "staging_ssm_role" {
+  name               = "${var.app}-staging"
+  assume_role_policy = data.aws_iam_policy_document.current_account_assume_role.json
+}
+
+module "staging_access" {
+  source      = "../../modules/ssm-access"
+  tag_key     = "Environment"
+  tag_value   = "staging"
+  policy_name = "${var.app}-staging"
+}
+
+
+resource "aws_iam_role_policy_attachment" "staging_ssm_user_attach" {
+  role       = aws_iam_role.staging_ssm_role.name
+  policy_arn = module.staging_user_policy.policy_arn
+}
+
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners = ["amazon"]
@@ -48,14 +80,8 @@ module "staging_instance" {
 }
 
 # Use the standard Amazon SSM policy to let the instances accept SSM sessions
-resource "aws_iam_role_policy_attachment" "ssm-attach" {
+resource "aws_iam_role_policy_attachment" "staging_ssm_instance_attach" {
   role       = module.staging_instance.instance_role_name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
-module "staging_access" {
-  source      = "../../modules/ssm-access"
-  tag_key     = "Environment"
-  tag_value   = "staging"
-  policy_name = "${var.app}-staging"
-}
