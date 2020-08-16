851f7e3060297f4cfaef937bc794580d95f26dd3
Jon Bass <jon@symops.io>
2020-08-16 18:46:35 -0400

    Add user access example

---
+++ examples/ssm-user-access/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.12.7"
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = var.aws_region
+}
+
+data "aws_caller_identity" "current" { }
+
+module "ssm_user_access" {
+  source                   = "../../modules/ssm-user-access"
+  policy_name              = var.app
+}
---
+++ examples/ssm-user-access/variables.tf
@@ -0,0 +1,8 @@
+variable "app" {
+  description = "Application name"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region to provision into"
+}
