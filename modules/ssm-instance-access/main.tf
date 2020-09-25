2602293518d0a8b28c9b42c37143a487017dd364
Jon Bass <jon@symops.io>
2020-09-24 20:06:16 -0400

    Use account id to identify bucket

--- modules/ssm-instance-access/main.tf
+++ modules/ssm-instance-access/main.tf
@@ -63,11 +63,13 @@ EOT
 
 data "aws_region" "current" {}
 
+data "aws_caller_identity" "current" {}
+
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "v1.9.0"
 
-  bucket        = "sym-ansible-${var.org_slug}-${data.aws_region.current.name}"
+  bucket        = "sym-ansible-${data.aws_caller_identity.current.account_id}-${data.aws_region.current.name}"
   acl           = "private"
 
   // S3 bucket-level Public Access Block configuration
--- modules/ssm-instance-access/variables.tf
+++ modules/ssm-instance-access/variables.tf
@@ -1,7 +1,3 @@
-variable "org_slug" {
-  description = "Org slug used in the Sym CLI"
-}
-
 variable "policy_name" {
   description = "The name of the IAM policy to create"
 }
